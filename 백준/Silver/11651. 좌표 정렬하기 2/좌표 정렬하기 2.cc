1af4b750a63b6a1efc6dc2ee60addd110e66527c
YEEUNco <121320706+YEEUNco@users.noreply.github.com>
2025-09-13 17:17:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 #include <algorithm>
+#include <utility>
 
 using namespace std;
 
-bool func(pair<int, int> a, pair<int, int> b ){
-    if(a.second == b.second){
-        return a.first<b.first;
-    }
-    else return a.second<b.second;
+bool func(pair<int, int> a, pair<int, int>b){
+    if(a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
 }
 
 int main(){
-    int num;
-    cin >> num;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    vector<pair<int,int>> v;
-    pair<int, int> myPair;
+    int n;
+    cin >> n;
 
-    for(int i=0; i<num; i++){
-        int num1, num2;
-        cin >> num1 >> num2;
-        myPair = {num1, num2};
-        v.push_back(myPair);
+    vector<pair<int, int>> v;
+    pair<int, int> p;
+    for(int i=0; i<n; i++){
+        int x, y;
+        cin >> x >> y;
+
+        p = {x, y};
+
+        v.push_back(p);
     }
-    sort(v.begin(), v.end(),func);
-    for(int i=0; i<num; i++) 
-        cout << v.at(i).first << " " << v.at(i).second << "\n";
+
+    sort(v.begin(), v.end(), func);
+    for(int i=0; i<n; i++){
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
 }
